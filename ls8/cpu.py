0026592fa0f4ce9d073c235b8c4f818e48f153d6
Mona LisaHeyClos <thiscarlos@gmail.com>
2020-06-18 15:47:56 -0700

    print 8.ls8 working

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,34 +2,62 @@
 
 import sys
 
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.branch_table = object()
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
 
         address = 0
 
+        with open(filename) as file:
+            for i in file:
+                command_split = i.split("#")
+                instruction = command_split[0]
+
+                if instruction == "":
+                    continue
+
+                first_bit = instruction[0]
+
+                if first_bit == "0" or first_bit == "1":
+                    self.ram[address] = int(instruction[:8], 2)
+                    address += 1
+
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+            
+        
+    def ram_read(self, mar):
+        return self.ram[mar]
 
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -62,4 +90,33 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        running = True
+
+        while running:
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            ir = self.ram_read(self.pc)  # Instruction register
+
+            if ir == LDI:
+                print("Hello!")
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+
+            elif ir == PRN:
+                value = self.reg[operand_a]
+                print(value)
+                self.pc += 2
+                # reg_num = self.ram[self.pc + 1]
+                # value = self.ram[self.pc + 2]
+                # self.reg[reg_num] = value
+                # self.pc += 3
+
+            elif ir == HLT:
+                running = False
+
+            else:
+                print(f'Unknown instruction {ir} at self.pc {self.pc}')
+                sys.exit(1)
+
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,8 @@
 import sys
 from cpu import *
 
-cpu = CPU()
+if len(sys.argv) == 2:
+    cpu = CPU()
 
-cpu.load()
-cpu.run()
+    cpu.load(sys.argv[1])
+    cpu.run()
